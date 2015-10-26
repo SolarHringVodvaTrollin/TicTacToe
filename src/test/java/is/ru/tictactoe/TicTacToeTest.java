37f7a7ccfe1ffcabf24527236029922a09afc267
Ragnar <ragnarmh13@ru.is>
2015-10-26 22:25:26 +0000

    Added base .java files for placeholders

---
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -0,0 +1,14 @@
+package is.ru.tictactoe;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TicTacToeTest {
+    // This test makes no sense, only put here as a placeholder.
+    @Test
+    public void TestHelloWorld() {
+        assertEquals("Hello world!", TicTacToe.main());
+    }
+}
+
