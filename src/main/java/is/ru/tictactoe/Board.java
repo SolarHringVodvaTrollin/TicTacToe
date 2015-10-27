e999c252a889eae167a19e5d9cb463c542a02e57
Ragnar Halldorsson <Ragnar Halldorsson>
2015-10-27 17:13:09 +0000

    Added base class skeletons

---
+++ src/main/java/is/ru/tictactoe/Board.java
@@ -0,0 +1,22 @@
+package is.ru.tictactoe;
+
+public class Board {
+	private Move[][] board;
+	private Player currentPlayer;
+
+	public void Board(Player p1, Player p2) {
+
+	}
+
+	public Player checkWinner() {
+
+	}
+
+	public Boolean insert(Move move) {
+
+	}
+
+	public Boolean isFull() {
+
+	}
+}
