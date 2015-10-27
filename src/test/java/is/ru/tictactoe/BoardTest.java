20d1ad3d5476c56d76a6e872f87bda0bbeca82ef
Ragnar <ragnarmh13@ru.is>
2015-10-27 23:18:29 +0000

    Wrote test for adding a move to an unoccupied spot on the board

--- src/test/java/is/ru/tictactoe/BoardTest.java
+++ src/test/java/is/ru/tictactoe/BoardTest.java
@@ -10,13 +10,23 @@ public class BoardTest {
 	public void testNewBoard() {
 		Board board = new Board();
 		Move[][] theBoard = board.getMoves();
-        for(int i = 0; i < 3; i++)
-        {
-        	for(int j = 0; j < 3; j++)
-        	{
-        		if(theBoard[i][j] != null) fail("One instance of the board wasn't null");
+        for(int i = 0; i < 3; i++) {
+        	for(int j = 0; j < 3; j++) {
+        		if(theBoard[i][j] != null) fail("A new board was created and a section wasn't null");
         	}
         }
+    }
+
+    @Test
+    public void testAddMove() {
+        Board board = new Board();
+        Move move = new Move(0, 0, new Player("Ragnar", "X"));
+
+        // Add a new move to the board
+        assertTrue(board.addMove(move));
+
+        // Check if the move is actually on the board
+        assertSame(move, board.getMoves()[0][0]);
     }
 }
 
