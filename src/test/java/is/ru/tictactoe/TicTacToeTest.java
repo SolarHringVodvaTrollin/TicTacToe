504f6c376603fddbed58923d71d2dc8c6cd995bc
benediktt14@ru.is <benediktt14@ru.is>
2015-10-27 14:12:06 +0000

    smavaegilegar breytingar

--- src/test/java/is/ru/tictactoe/TicTacToeTest.java
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TicTacToeTest {
+	
     @Test
     public void TestGreet() {
         assertEquals("Hello world!", TicTacToe.greet());
--- src/test/java/is/ru/tictactoe/UserInterfaceTest.java
+++ src/test/java/is/ru/tictactoe/UserInterfaceTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 public class UserInterfaceTest {
 	UserInterface userInterface = new UserInterface();
+	
 	@Test
     public void TestShow() {
         assertEquals("__|__|__\n__|__|__\n__|__|__", userInterface.show());
