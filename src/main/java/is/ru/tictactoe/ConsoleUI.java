b81e907295cb705bf7f8ac9bf6940e62c5e386bc
benediktt14@ru.is <benediktt14@ru.is>
2015-10-28 14:48:11 +0000

    baetti vid 2 draw follum, 1 sem tekur vid breytu en hitt sem byr til interfacid i byrjun

--- src/main/java/is/ru/tictactoe/ConsoleUI.java
+++ src/main/java/is/ru/tictactoe/ConsoleUI.java
@@ -2,10 +2,41 @@ package is.ru.tictactoe;
 
 public class ConsoleUI implements UI {
 	public void draw() {
-		System.out.println("Hello world!");
+		System.out.println("   |   |   ");
+		System.out.println("   |   |   ");
+		System.out.println("   |   |   ");
+	}
+
+	public void draw(Board board) {
+		Move move = board.getMoves();
+
+		for(int i = 0; i < move.length; i++){
+			for(int j = 0; j < move.length; j++){
+				if(j == 1){
+					if(move[i][j] != null){
+						Player player = move[i][j].getPlayer();
+						System.print("| " + player.getSymbol()  + " |");
+					}
+					else{
+						System.print("|   |");
+					}
+				}
+				else{
+					if(move[i][j] != null){
+						Player player = move[i][j].getPlayer();
+						System.print(" " + player.getSymbol()  + " ");
+					}
+					else{
+						System.print("   ");
+					}
+				}
+			}
+		}
 	}
 
 	public void getInput() {
 		System.out.println("Enter numbers:");
 	}
+
+
 }
