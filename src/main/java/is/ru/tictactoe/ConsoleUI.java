5ec7700d5b7f5371ad9e3c380bf47c72100b8f9a
benediktt14@ru.is <benediktt14@ru.is>
2015-10-28 15:24:54 +0000

    consoleui sem virkar

--- src/main/java/is/ru/tictactoe/ConsoleUI.java
+++ src/main/java/is/ru/tictactoe/ConsoleUI.java
@@ -1,36 +1,38 @@
 package is.ru.tictactoe;
 
-public class ConsoleUI implements UI {
+public class ConsoleUI {
 	public void draw() {
 		System.out.println("   |   |   ");
 		System.out.println("   |   |   ");
 		System.out.println("   |   |   ");
 	}
 
-	public void draw(Board board) {
-		Move move = board.getMoves();
+	public static void draw(Board board) {
+		Move[][] move = board.getMoves();
 
 		for(int i = 0; i < move.length; i++){
 			for(int j = 0; j < move.length; j++){
-				if(j == 1){
+
+				if(j == 1){ //Tegar j er 1 ta baetum vid vid linu til ad afmarka interfacid
 					if(move[i][j] != null){
 						Player player = move[i][j].getPlayer();
-						System.print("| " + player.getSymbol()  + " |");
+						System.out.print("| " + player.getSymbol()  + " |");
 					}
 					else{
-						System.print("|   |");
+						System.out.print("|   |");
 					}
 				}
-				else{
+				else{ //Engar linur til ad afmarka interfacid
 					if(move[i][j] != null){
 						Player player = move[i][j].getPlayer();
-						System.print(" " + player.getSymbol()  + " ");
+						System.out.print(" " + player.getSymbol()  + " ");
 					}
 					else{
-						System.print("   ");
+						System.out.print("   ");
 					}
 				}
 			}
+			System.out.println();
 		}
 	}
 
