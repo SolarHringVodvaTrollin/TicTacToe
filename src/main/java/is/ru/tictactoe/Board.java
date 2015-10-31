b70c76ee10c3e219d5c4e2352a253781f8903a9d
Ragnar <ragnarmh13@ru.is>
2015-10-31 02:25:04 +0000

    Fixed build errors in Board as a result of changes to Move

--- src/main/java/is/ru/tictactoe/Board.java
+++ src/main/java/is/ru/tictactoe/Board.java
@@ -1,16 +1,11 @@
 package is.ru.tictactoe;
 
 public class Board {
-	private Move[][] board;
+	private Move[] board;
 	private int moveCount;
 
 	public Board() {
-		board = new Move[3][3];
-		for (int i = 0; i < board.length; i++) {
-			for(int j = 0; j < board.length; j++) {
-				board[i][j] = null;
-			}
-		}
+		board = new Move[9];
 		moveCount = 0;
 	}
 
@@ -19,19 +14,31 @@ public class Board {
 	 * 
 	 * @return an array of Moves currently on the board
 	 */
-	public Move[][] getMoves() {
+	public Move[] getMoves() {
 		return board;
 	}
 
+	/**
+	 * Gets the move currently on given (zero-indexed) board square
+	 *
+	 * @return the Move at square or null if square is empty
+	 */
+	public Move getMoveAt(int square) {
+		if(square < 0 || square > 8) {
+			return null;
+		}
+
+		return board[square];
+	}
+
 	public Boolean addMove(Move move) {
-		int x = move.getX();
-		int y = move.getY();
+		int square = move.getSquare();
 
 		// Refuse overwriting a move with a new move
-		if(board[x][y] != null)
+		if(board[square] != null)
 			return false;
 
-		board[x][y] = move;
+		board[square] = move;
 		moveCount++;
 		return true;
 	}
--- src/test/java/is/ru/tictactoe/BoardTest.java
+++ src/test/java/is/ru/tictactoe/BoardTest.java
@@ -21,13 +21,10 @@ public class BoardTest {
     public void testGetMoves() {
         Board board = new Board();
 
-        Boolean x = true;
-        Boolean o = false;
-
-        Move move1 = new Move(0, x);
-        Move move2 = new Move(2, o);
-        Move move3 = new Move(4, x);
-        Move move4 = new Move(7, o);
+        Move move1 = new Move(0, true);
+        Move move2 = new Move(2, false);
+        Move move3 = new Move(4, true);
+        Move move4 = new Move(7, false);
 
         board.addMove(move1);
         board.addMove(move2);
@@ -49,21 +46,34 @@ public class BoardTest {
     }
 
     @Test
+    public void testGetMoveAt() {
+        Board board = new Board();
+
+        assertNull(board.getMoveAt(-1));
+        assertNull(board.getMoveAt(9));
+
+        Move move = new Move(3, true);
+        board.addMove(move);
+
+        assertSame(move, board.getMoveAt(3));
+    }
+
+    @Test
     public void testAddMove() {
         Board board = new Board();
-        Move move = new Move(0, 0, new Player("Turing", true));
+        Move move = new Move(0, true);
 
         // Add a new move to the board, should return true because unoccupied
         assertTrue(board.addMove(move));
         // Check if the move is actually on the board
-        assertSame(move, board.getMoves()[0][0]);
+        assertSame(move, board.getMoves()[0]);
 
-        Move move2 = new Move(0, 0, new Player("Lovelace", false));
+        Move move2 = new Move(0, false);
 
         // Try adding a new move to an occupied space
         assertFalse(board.addMove(move2));
         // Verify that the original move was not overwritten
-        assertSame(move, board.getMoves()[0][0]);
+        assertSame(move, board.getMoves()[0]);
     }
 
     @Test
