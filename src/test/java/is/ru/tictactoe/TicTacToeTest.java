ea475c130f2b171bced049344cd769acfcd813e2
Ragnar <ragnarmh13@ru.is>
2015-10-31 01:10:27 +0000

    Added test for getPlayer() method in TicTacToe

--- src/test/java/is/ru/tictactoe/TicTacToeTest.java
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -1,6 +1,6 @@
 package is.ru.tictactoe;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -10,5 +10,18 @@ public class TicTacToeTest {
     public void TestGreet() {
         assertEquals("Hello world!", TicTacToe.greet());
     }
-}
 
+    @Test
+    public void testGetPlayer() {
+    	TicTacToe game = new TicTacToe();
+
+    	Player player1 = game.getPlayer(true);
+    	Player player2 = game.getPlayer(false);
+
+    	assertEquals("Player 1", player1.getName());
+    	assertEquals("Player 2", player2.getName());
+
+    	assertTrue(player1.getSymbol());
+    	assertTrue(player2.getSymbol());
+    }
+}
